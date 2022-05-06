c2ada060e0f7a008f440db38a9c4f7e12b9f3b91
Nragis <quinn_murphey@me.com>
2022-05-05 21:14:07 -0500

    Initialized package

---
+++ PfDA/setup.py
@@ -0,0 +1,12 @@
+from gettext import install
+from setuptools import setup
+
+setup(
+    name='PfDA',
+    version='0.0',
+    description='A package containing code for my work through PfDA',
+    author='Quinn Murphey',
+    author_email='Quinn_Murphey@me.com',
+    packages=['PfDA'],
+    install_requires=['pandas', 'numpy']
+)
